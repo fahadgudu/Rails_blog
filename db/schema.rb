f06cce9ff402e0595554b25979b91935ca7180a5
zakrevitz <zakrevitz@gmail.com>
2015-10-07 17:00:18 +0300

    Minor changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151002091157) do
+ActiveRecord::Schema.define(version: 20151006095042) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,8 +52,16 @@ ActiveRecord::Schema.define(version: 20151002091157) do
     t.string   "title"
     t.string   "author"
     t.text     "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+    t.integer  "tag_ids",    default: [],              array: true
+  end
+
+  add_index "posts", ["tag_ids"], name: "index_posts_on_tag_ids", using: :gin
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.string "slug"
   end
 
 end
