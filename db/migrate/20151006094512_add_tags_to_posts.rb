6766f0bae704e70d3f2d3f0f3d052d6a122bd2d8
zakrevitz <zakrevitz@gmail.com>
2015-10-11 20:54:39 +0300

    Migrations

---
+++ db/migrate/20151006094512_add_tags_to_posts.rb
@@ -0,0 +1,6 @@
+class AddTagsToPosts < ActiveRecord::Migration
+  def change
+    add_column :posts, :tag_ids, :integer, array: true, default: []
+    add_index :posts, :tag_ids, using: 'gin'
+  end
+end
